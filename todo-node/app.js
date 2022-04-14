4a948ca015fadb4ad01abd9f11e786b3b9a3c0de
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-14 13:35:48 +0530

    added package json,restart the application automatically, read environment variable from node.js

--- todo-node/app.js
+++ todo-node/app.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const http = require('http')
 const hostname = '127.0.0.1';
 const port = 30000;
 
+console.log(process.env.USER_ID);
+console.log(process.env.USER_KEY);
+
 const server = http.createServer(
     (req, res) => {
         console.log(req.headers);
-        console.log("-------------------------------")
         res.statusCode = 200;
         res.setHeader('content-type', 'text/plain');
+        res.setHeader('thbs-type', 'angular')
         res.end('welcome to thbs');
     }
 )
