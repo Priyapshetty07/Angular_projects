bed23bd7f3fe78c9b87708c30107b3714becad52
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-19 13:58:53 +0530

    added express

--- todo-node/demo.js
+++
@@ -1,7 +0,0 @@
-console.log(`taking cmd line arg for node js program`);
-process.argv.forEach((ele, index, arr) => {
-    console.log(`${ele} --> ${index}`);
-})
-
-const args = process.argv.slice(3);
-console.log(args);
