fcb422f5a10cd3a4d8acf19e19904800f67e010d
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-21 01:04:14 +0530

    support using api mysql

--- support-ticket/routes/users.js
+++ support-ticket/routes/users.js
@@ -10,43 +10,7 @@ router.get('/', function (req, res, next) {
     res.send(results);
   });
 });
-
 // post method
-// router.post('/tickets', (req, res, next) => {
-
-//   let {
-// id,
-// username,
-// ticketid,
-// ticketQuestion,
-// ticket_date
-// } = req.body
-
-//   // let insertcommand = `INSERT INTO support_ticket.ticket 
-//   // (id, username, ticketid, ticketQuestion,ticket_date)
-//   //         VALUES (${id},'${username}',${ticketid},'${ticketQuestion}','${ticket_date}')`;
-
-
-
-//   dbConnection.query(insertcommand, (err, result) => {
-//     if (err) throw err;
-//     res.send("detailes are fetched from postman api detailes are inserted");
-//   });
-// });
-
-// router.get('/ticket', function (req, res, next) {
-
-//   dbConnection.query('SELECT * FROM support_ticket.ticket', (error, results, fields) => {
-//     if (error) throw error;
-//     res.send(results);
-//   });
-//   //res.send('respond with a resource');
-// });
-
-
-
-
-
 router.post('/tickets', (req, res, next) => {
 
   let {
