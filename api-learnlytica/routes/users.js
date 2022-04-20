255f75cc9a2ccec1cefa0acd32a3b5e9b55bf5a0
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-20 17:26:02 +0530

    added the  record in mysql express calling api

--- api-learnlytica/routes/users.js
+++ api-learnlytica/routes/users.js
@@ -14,5 +14,53 @@ router.get('/', function (req, res, next) {
 });
 
 
+ 
+
+router.post('/profile', (req, res, next) => {
+
+  let {
+    username,
+    email,
+    password,
+    first_name,
+    last_name,
+    job_title,
+    salary,
+    create_datetime
+  } = req.body
+
+  // console.log(req.body);
+
+ 
+let insertcommand = `INSERT INTO user_profile (username, email, password, first_name,last_name,job_title,salary,create_datetime) VALUES ('${username}','${email}','${password}','${first_name}','${last_name}','${job_title}','${salary}','${create_datetime}')`;
+
+  dbConnection.query(insertcommand, (err, result) => {
+
+    if (err) throw err;
+
+    res.send("detailes are fetched from postman api detailes are inserted");
+
+  });
+
+})
+
+router.get('/profile', function (req, res, next) {
+
+
+
+  dbConnection.query('SELECT * FROM api_learn.user_profile', (error, results, fields) => {
+
+    if (error) throw error;
+
+    res.send(results);
+
+
+
+  });
+
+  //res.send('respond with a resource');
+
+});
+
 
 module.exports = router;
