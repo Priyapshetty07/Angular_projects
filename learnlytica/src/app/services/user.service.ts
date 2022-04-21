50c0bbd0c0b81ca9bf85d267aee128d516687efa
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-21 10:05:39 +0530

    connect angular app to express backend

--- learnlytica/src/app/services/user.service.ts
+++ learnlytica/src/app/services/user.service.ts
@@ -6,9 +6,18 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
- constructor(private myHttp: HttpClient) { }
- 
+  constructor(private myHttp: HttpClient) { }
+
   getUserProfile() {
     return this.myHttp.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8')
+  }
+
+  // getProfiles() {
+
+  //   this.myHttp.get('http://localhost:4200/api/users').subscribe((data) => {
+  //     console.log(data)
+  //   });
+  getProfiles() {
+    return this.myHttp.get('http://localhost:4200/api/users') //obs
   }
 }
