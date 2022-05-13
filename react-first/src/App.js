828504ee358a7ee2443e44161ac1f75eff83da7c
Priyapshetty07 <priyapshetty007@gmail.com>
2022-05-13 16:31:03 +0530

    added components

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Contact from './Contact'
+import image1 from './images/image1.png'
+
+
 
 function App() {
+
+ 
+
+  let myTitle = "Welcome to react js learning camp";
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <>
+
+      <header>
+
+        <img src={image1} width="100%" height="200" ></img>
+
+        {/* this is calling of function component of react js  */}
+
+        <Contact></Contact>
+
       </header>
-    </div>
+
+      <div className="thbs-text">
+
+        <h2>{myTitle}</h2>
+
+        <Contact></Contact> 
+
+      </div>
+
+    </>
+
   );
+
 }
 
 export default App;
