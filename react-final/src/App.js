6e111c7eb4752c1a5132ee7c9840b8921d936a2a
Priyapshetty07 <priyapshetty007@gmail.com>
2022-05-18 11:48:19 +0530

    added layout compoents and status to the project

--- react-final/src/App.js
+++ react-final/src/App.js
@@ -4,14 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import UserPage from './pages/UserPage';
 
+import HeaderLayout from './layout/HeaderLayout';
+import FooterLayout from './layout/FooterLayout';
+
 export default function App() {
   return (
     <div className="App">
       <h1>Routes</h1>
+      <HeaderLayout></HeaderLayout>
         <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/user" element={<UserPage />} />
       </Routes>
+      <FooterLayout></FooterLayout>
     </div>
   );
 }
---
+++ react-final/src/layout/HeaderLayout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+{/* <HeaderLayout title="dadsa"></HeaderLayout> */ }
+export default class HeaderLayout extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            URL: "www.thbs.com",
+            name: "test"
+        };
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    {this.props.title}
+                    {this.state.URL} -- {this.state.name}
+                </div>
+                <ul>
+                    <li>
+                        <Link to="/">HomePage </Link>
+                        <br></br>
+                        <Link to="/user">UserPage</Link>
+                    </li>
+                </ul>
+            </>
+        )
+    }
+
+}
