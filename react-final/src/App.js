1dbbb7012b4f159f70e51095dd34a4468632a768
Priyapshetty07 <priyapshetty007@gmail.com>
2022-05-18 11:40:02 +0530

    added pages and layout files

--- react-final/src/App.js
+++ react-final/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
 
 export default function App() {
   return (
     <div className="App">
-     <h1>Router</h1>
+      <h1>Routes</h1>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
     </div>
   );
 }
